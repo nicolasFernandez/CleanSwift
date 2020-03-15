6e711978fb09674c97aea52688dee1a78a41e658
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2020-03-15 11:26:34 -0300

    Initial Commit

---
+++ CleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwift
+//
+//  Created by Nicolás Fernández Alarcón on 15-03-20.
+//  Copyright © 2020 Nicolás Fernández Alarcón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
